10fdeb8d2a6f3f5df6861eebaf2b1dad1d4f43f1
yurir <yuri.ratner@gmail.com>
2022-01-22 10:58:21 -0500

    cleanup

--- main.cpp
+++ main.cpp
@@ -1,30 +1,9 @@
 
-
-//#include "tests.h"
-
 #include <iostream>
 
 
 int main(int argc, char* argv[])
 {
-	//size_t len = 100;
-	//for (size_t i = 0; i < len; i++)
-	//{
-	//	if (!testInterface())
-	//		break;
-	//
-	//	if (!testM2OQueue_ManyThreads_Pushes_1thread_Pops())
-	//		break;
-	//	if (!testM2OQueue_1thread_Pops_ManyThreads_Pushes())
-	//		break;
-	//	if (!testM2MQueue_ManyThreads_Pushes_ManyThreads_Pops())
-	//		break;
-	//	if (!testM2MQueue_ManyThreads_Pops_ManyThreads_Pushes())
-	//		break;
-	//	if (!testM2MQueue_ManyThreads_Pushes_1thread_Pops())
-	//		break;
-	//
-	//	std::cout << "iteration: " << i << ":" << len << " finished" << std::endl << std::endl;
-	//}
+	std::cout << "hello world, everything is in tests/test*.cpp" << std::endl;
 	return 0;
 }
